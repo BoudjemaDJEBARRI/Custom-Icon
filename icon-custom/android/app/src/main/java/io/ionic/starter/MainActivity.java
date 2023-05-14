cf4871a981fdb0a4696a019e6638b6d8d73aa687
Boudjema DJEBARRI <boudjema.djebari@gmail.com>
2023-05-14 17:10:15 +0200

    Deploy

---
+++ icon-custom/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -0,0 +1,5 @@
+package com.contactsApp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- icon-custom/src/app/home/home.page.ts
+++ icon-custom/src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Contacts } from '@capacitor-community/contacts';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
 
+ ngOnInit() {
+  this.getContacts();
+  }
+
+  async getContacts () {
+
+    try {
+      const permission = await Contacts.requestPermissions();
+      console.log('permission: ', permission.contacts)
+
+    } catch(e) {
+      console.log(e);
+    }
+
+
+  }
 }
